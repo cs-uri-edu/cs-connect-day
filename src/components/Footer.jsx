7f34b696376048eafc5770321fdf27ec80ed7ca0
sarmenti7 <sarmenti@uri.edu>
2026-07-09 14:11:03 -0400

    Finalized footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,65 +7,56 @@ import { FaXTwitter } from 'react-icons/fa6';
 function Footer() {
     return (
         <footer className="footer">
+            <div className="footer__content">
 
-            <div className="footer__left">
-                <a
-                    href="https://www.uri.edu/"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    University of Rhode Island
-                </a>
+                <div className="footer__column">
+                    <h2>Event</h2>
 
-                <a
-                    href="https://web.uri.edu/cs/"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    Department of Computer Science and Statistics
-                </a>
-            </div>
+                    <p>September 18, 2026</p>
+                    <p>Memorial Union Ballroom, 50 Lower College Rd, Kingston, RI 02881</p>
+                    <a href="mailto:csconnect@uri.edu">csconnect@uri.edu</a>
+                </div>
 
-            <div className="footer__right">
+                <div className="footer__column">
+                    <h2>Department</h2>
 
-                <a 
-                    href="https://app.joinhandshake.com/stu/events/1957249" 
-                    aria-label="Handshake URI CS Connect Day Event"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    <img src={handshakeLogo} alt="Handshake" />
-                </a>
+                    <a href="https://web.uri.edu/cs/" target="_blank" rel="noreferrer">
+                        Department of Computer Science and Statistics
+                    </a>
 
-                <a
-                    href="https://www.facebook.com/uri.ccee/"
-                    aria-label="URI CCEE Facebook"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    <FaFacebookF />
-                </a>
+                    <a href="https://career.uri.edu/" target="_blank" rel="noreferrer">
+                        Center for Career & Experiential Education
+                    </a>
 
-                <a
-                    href="https://www.instagram.com/uriccee/"
-                    aria-label="URI CCEE Instagram"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    <FaInstagram />
-                </a>
+                </div>
 
-                <a
-                    href="https://x.com/URICCEE"
-                    aria-label="URI CCEE X"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    <FaXTwitter />
-                </a>
+                <div className="footer__column footer__column--social">
+                    <h2>Connect</h2>
+
+                    <div className="footer__socials">
+                        <a href="https://app.joinhandshake.com/stu/events/1957249" aria-label="Handshake URI CS Connect Day Event" target="_blank" rel="noreferrer">
+                            <img src={handshakeLogo} alt="Handshake" />
+                        </a>
+
+                        <a href="https://www.facebook.com/uri.ccee/" aria-label="URI CCEE Facebook" target="_blank" rel="noreferrer">
+                            <FaFacebookF />
+                        </a>
+
+                        <a href="https://www.instagram.com/uriccee/" aria-label="URI CCEE Instagram" target="_blank" rel="noreferrer">
+                            <FaInstagram />
+                        </a>
+
+                        <a href="https://x.com/URICCEE" aria-label="URI CCEE X" target="_blank" rel="noreferrer">
+                            <FaXTwitter />
+                        </a>
+                    </div>
+                </div>
 
             </div>
 
+            <div className="footer__bottom">
+                <p>© 2026 URI CS Connect Day</p>
+            </div>
         </footer>
     );
 }
